46a2ca200714b0156c4ac959bcee9deaa881e2a3
Naim-Kissi <naim.kissi@gmail.com>
2015-02-15 14:19:42 +0100

    La vue

--- Mock/Machine.java
+++ Mock/Machine.java
@@ -1,39 +1,39 @@
+import java.util.Observer;
+import java.util.Observable;
 
-public class Machine {
-	
+public class Machine extends Observable {
 	private String[] tabRotor;
 	private String reflecteur;
 	private String plug;
-	
 	public Machine(){
 		this.tabRotor=new String[3];
 		this.tabRotor[0]="Rotor 1";
 		this.tabRotor[1]="Rotor 2";
 		this.tabRotor[2]="Rotor 3";
-		
 		this.plug="plugboard";
 		this.reflecteur="reflecteur";
-		
-	
 	}
-	
 	public char crypter(char c){
 		return 'z';
 	}
-	
 	public String decrypter(String c){
 		return "chaine decryptee";
-	}	
-	
+	}
 	public String getPlugboard(){
 		return "get: Plugboard";
 	}
-	
 	public String getRotor(int i){
 		return "get: Rotor "+i;
 	}
+
+	public String getPosRotor(int i){
+		return ""+(i+1)+"";
+	}
+
+	public void up(){
+		setChanged();
+		notifyObservers();
 	
-	
-	
+	}
 
 }
--- Mock/Main.java
+++ Mock/Main.java
@@ -1,16 +1,18 @@
 import java.awt.Dimension;
+import java.util.Observer;
+import java.util.Observable;
 import java.awt.Toolkit;
-
 
 public class Main {
 	public static void main(String[] args) {
-		Toolkit aTK= Toolkit.getDefaultToolkit();
-		Dimension dim = aTK.getScreenSize();
-		int large=dim.width;
-		int haut=dim.height;
-	
-		Vue v=new Vue(large, haut);
-		Machine m=new Machine();
-		Controleur controleur=new Controleur(v,m);
+			Toolkit aTK= Toolkit.getDefaultToolkit();
+			Dimension dim = aTK.getScreenSize();
+			int large=dim.width;
+			int haut=dim.height;
+
+			Machine m=new Machine();
+			Vue v=new Vue(large, haut,m);
+			Controleur controleur=new Controleur(v,m);
+			m.up();
 	}
 }
