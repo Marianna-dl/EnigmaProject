543c1a78ff2b954efe26373be8754f80f3dbd1dd
Rtrouve <rtrouve@hotmail.fr>
2015-02-23 16:35:22 +0100

    Update Reflecteur.java

--- Projet/Reflecteur.java
+++ Projet/Reflecteur.java
@@ -2,7 +2,7 @@ public class Reflecteur {
 	private int[] tabCouple;
 	
 	public Reflecteur(){
-		this.tabCouple = new int[]{13,7,21,16,17,12,15,1,18,10,9,24,5,0,25,6,3,4,8,22,23,2,19,20,11,14};
+		this.tabCouple = new int[]{39,23,27,12,7,30,5,35,13,38,21,4,9,25,44,32,20,29,42,17,11,46,2,33,14,45,3,18,34,6,36,16,24,29,8,31,43,10,41,1,39,19,37,15,26,22};
 	}
 	public int getCorrespondance(int i){
 		return tabCouple[i];
