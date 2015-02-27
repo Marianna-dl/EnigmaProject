59debf7b8050ac28d84c10c802b0a4cf93310e0c
Rtrouve <rtrouve@hotmail.fr>
2015-02-27 10:35:54 +0100

    Update Machine.java

--- Projet/Machine.java
+++ Projet/Machine.java
@@ -6,7 +6,7 @@ public class Machine extends Observable {
 	private Plugboard plugboard;
 	private Rotor[] tabRotor;
 	private Reflecteur reflecteur;
-	static final char[] CONVERT = {(char)0,'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','.','!','?',',',':','(',')','\'','"'};
+	static final char[] CONVERT = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','.','!','?',',',':','(',')','\'','"'};
 	
 	public Machine (Rotor[] tabR, Reflecteur ref, Plugboard plug){
 		this.plugboard=plug;
@@ -14,7 +14,7 @@ public class Machine extends Observable {
 		this.reflecteur=ref;
 	}
 	public char crypter (char c){
-		int i= 1,nbLetter = 0;
+		int i= 0,nbLetter = 0;
 		while(i<CONVERT.length && c!=CONVERT[i]){
 			i++;
 		}
